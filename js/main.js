a6a6fe05a612f064c1461def5db8adf4732a5fdf
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-28 16:35:35 +0100

    Logica javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(document).ready( function() {
-
+  // REFERENZE
+  var dropdown = $('.navigazione .destra .with-dropdown > a');
+  var menu = $('.navigazione .destra .dropdown-menu');
+  // EVENTO
+  dropdown.click( function() {
+    var actualMenu = $(this).next();
+    menu.not(actualMenu).hide();
+    $(this).next().toggle();
+  });
 
 
 }); //<-----Fine Ready
